1190ed46179d0bd11c9a5d536628339460bcc21b
guchimina -thinkpad <kazu9sum.mi@gmail.com>
2022-02-23 17:48:15 +0900

    p124

---
+++ kernel/.font.d
@@ -0,0 +1,5 @@
+font.o: font.cpp font.hpp \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  graphics.hpp frame_buffer_config.hpp
---
+++ kernel/.main.d
@@ -0,0 +1,7 @@
+main.o: main.cpp \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/cstdint \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/__config \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  frame_buffer_config.hpp graphics.hpp font.hpp
