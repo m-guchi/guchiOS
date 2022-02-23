ca336314f223817656df27fb5f8e673ff83abaab
guchimina -thinkpad <kazu9sum.mi@gmail.com>
2022-02-23 17:20:55 +0900

    p121

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -3,6 +3,25 @@
 
 #include "frame_buffer_config.hpp"
 
+const uint8_t kFontA[16] = {
+  0b00000000,
+  0b00011000,
+  0b00011000,
+  0b00011000,
+  0b00011000,
+  0b00100100,
+  0b00100100,
+  0b00100100,
+  0b00100100,
+  0b01111110,
+  0b01000010,
+  0b01000010,
+  0b01000010,
+  0b11100111,
+  0b00000000,
+  0b00000000,
+};
+
 struct PixelColor {
   uint8_t r, g, b;
 };
@@ -45,6 +64,21 @@ class BGRResv8BitPerColorPixelWriter : public PixelWriter {
   }
 };
 
+
+// 5.1
+void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color){
+  if (c != 'A'){
+    return;
+  }
+  for(int dy=0; dy<16; ++dy){
+    for(int dx=0; dx<8; ++dx){
+      if((kFontA[dy]<<dx) & 0x80u){
+        writer.Write(x+dx, y+dy, color);
+      }
+    }
+  }
+}
+
 void* operator new(size_t size, void* buf){
   return buf;
 }
@@ -77,5 +111,7 @@ extern "C" void KernelMain(const FrameBufferConfig& frame_buffer_config){
     }
   }
 
+  WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
+  WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
   while(1) __asm__("hlt");
 }
