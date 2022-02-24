e5f27f449be3fe4dd58742b38a3b61c3f125369d
guchimina -thinkpad <kazu9sum.mi@gmail.com>
2022-02-24 11:42:05 +0900

    p128

--- kernel/.main.d
+++ kernel/.main.d
@@ -4,4 +4,6 @@ main.o: main.cpp \
   /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/stdint.h \
   /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/cstddef \
   /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/version \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/cstdio \
+  /home/guchi/osbook/devenv/x86_64-elf/include/c++/v1/stdio.h \
   frame_buffer_config.hpp graphics.hpp font.hpp
